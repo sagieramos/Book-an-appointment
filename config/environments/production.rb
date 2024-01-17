433c7b88c97c9c6b7b2a6ae256c63bf6f473cd29
Stanley Osagie Ramos <sagiecyber@gmail.com>
2024-01-17 22:13:38 +0100

    Sendgrid confit confit in dev and prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,23 @@ Rails.application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
+  
+  
+  
+  # Configuration for ActionMailer using SendGrid
+  
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #address: 'smtp.sendgrid.net',
+  #port: 587,
+  #domain: 'your-production-domain.com',
+  #user_name: ENV['SENDGRID_USERNAME'], #use apikey
+  #password: ENV['SENDGRID_PASSWORD'],
+  #authentication: :plain,
+  #enable_starttls_auto: true,
+#}
+
+
 
   # Don't log any deprecations.
   config.active_support.report_deprecations = false
@@ -87,4 +104,6 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  
+
 end
