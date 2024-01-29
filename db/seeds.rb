e800fcba0c84d86345f19d5be9725daef280103b
Stanley Osagie Ramos <sagiecyber@gmail.com>
2024-01-29 08:45:34 +0100

    Make super_admin an admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@ user = User.new(
   email:  's@g.c', #ENV['SUPER_ADMIN_EMAIL'],
   username: 's_admin', #ENV['SUPER_ADMIN_USERNAME'],
   super_admin: true,
+  admin: true,
   first_name: 'Stanley', #ENV['SUPER_ADMIN_FIRST_NAME'],
   last_name: 'Osagie' #ENV['SUPER_ADMIN_LAST_NAME']
 )
