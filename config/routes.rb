404c50913b1e063d3bd2d50e0f03946a24aa29ae
nilab-khwaja <nilab.khwajazada1212@gmail.com>
2024-02-01 12:46:06 +0430

    add initlaizers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount Rswag::Api::Engine => '/api-docs'
+  mount Rswag::Ui::Engine => '/api-docs'
   namespace :api do
     namespace :v1 do
       resources :pages, path: 'p' do
